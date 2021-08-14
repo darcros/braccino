4c2c21dbe27079e669795f8d2233889ef85ec874
darcros <darcros7@gmail.com>
2021-08-14 16:57:33 +0200

    generate nerves firmware

---
+++ braccino_firmware/lib/braccino_firmware.ex
@@ -0,0 +1,18 @@
+defmodule BraccinoFirmware do
+  @moduledoc """
+  Documentation for BraccinoFirmware.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BraccinoFirmware.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ braccino_firmware/test/braccino_firmware_test.exs
@@ -0,0 +1,8 @@
+defmodule BraccinoFirmwareTest do
+  use ExUnit.Case
+  doctest BraccinoFirmware
+
+  test "greets the world" do
+    assert BraccinoFirmware.hello() == :world
+  end
+end
