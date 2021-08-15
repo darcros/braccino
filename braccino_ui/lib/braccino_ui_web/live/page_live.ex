89d3df2e953d312e62b3ddd833d3d4432b684b0b
darcros <darcros7@gmail.com>
2021-08-15 15:57:22 +0200

    implement live view

--- braccino_ui/lib/braccino_ui_web/live/page_live.ex
+++ braccino_ui/lib/braccino_ui_web/live/page_live.ex
@@ -1,39 +1,50 @@
 defmodule BraccinoUiWeb.PageLive do
   use BraccinoUiWeb, :live_view
 
+  alias Braccino.Braccio
+  alias Braccino.Braccio.Angles
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    if Braccio.connected?() do
+      :ok = Braccio.disconnect()
+    end
+
+    send(self(), :connect)
+    {:ok, assign(socket, angles: %Angles{}, status: :connecting)}
   end
 
   @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+  def handle_info(:connect, socket) do
+    :ok = Braccio.connect()
+
+    # set the initial angles
+    :ok = Braccio.set_angles(socket.assigns.angles)
+
+    {:noreply, assign(socket, status: :connected)}
   end
 
   @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
+  def handle_event("angles", params, socket) do
+    # params are passed as strings, so we need to parse them
+    # it is very important that we check the name before coverting it to an atom
+    # TODO: use ecto to cast the parameters instead of doing it by hand
+    parsed_params =
+      for {name, angle} <- params,
+          name in [
+            "base",
+            "shoulder",
+            "elbow",
+            "wrist_ver",
+            "wrist_rot",
+            "gripper"
+          ],
+          into: %{},
+          do: {String.to_atom(name), String.to_integer(angle)}
 
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
-  end
+    {:ok, angles} = Angles.new(parsed_params)
+    :ok = Braccio.set_angles(angles)
 
-  defp search(query) do
-    if not BraccinoUiWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+    {:noreply, assign(socket, angles: angles)}
   end
 end
